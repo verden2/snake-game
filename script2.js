1440e0eb085227415cfb87ac8c07a9d95b9c5403
verden2 <155012457+verden2@users.noreply.github.com>
2024-10-12 10:16:42 +0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -7,7 +7,7 @@ document.getElementById('login-form').addEventListener('submit', async function 
     // Проверяем логин и пароль
     if (username === 'root' && password === 'toor') {
         // Если логин и пароль правильные, перенаправляем на index.html
-        window.location.href = "./snake.html"; 
+        window.location.href = "./button.html"; 
     }
     // Если логин или пароль неверные, ничего не происходит
 });
